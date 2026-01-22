86cd57c3f13b705555c239581277392427e8dfd0
Eldax <mohamedabu.el3ela@gmail.com>
2026-01-22 17:36:25 +0200

    feat(Infrastructure): Publishing domain events

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -1,14 +1,16 @@
 using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
 public class ApplicationDbContext : DbContext , IUnitOfWork
 {
+    private readonly IPublisher _publisher;
 
-    public ApplicationDbContext(DbContextOptions options) : base(options)
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher) : base(options)
     {
-        
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -16,4 +18,40 @@ public class ApplicationDbContext : DbContext , IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly); // automatically applies all configurations
         
     }
+
+    public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        await PublishDomainEventsAsync();
+        int result =  await base.SaveChangesAsync(cancellationToken);
+        return result;
+    }
+
+    public async Task PublishDomainEventsAsync()
+    {
+        // for each entity get its domain events , and publish all in the end.
+
+        List<IDomainEvent> domainEvents = ChangeTracker.Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                // get the domain events of the entity
+                IReadOnlyList<IDomainEvent> domainEvents = entity.GetDomainEvents();
+                
+                // after getting the domain events , clear them 
+                entity.ClearDomainEvents();
+                
+                return domainEvents;
+            })
+            .ToList();
+        
+        // no we have a list of all the domain events occured in all entites
+        // we need to handle them
+
+        foreach (IDomainEvent domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
+    }
+
+
 }
